5edcebc6a41ba085ac5f96fffe5795af5201a9f5
Nick Moore <nick_r_moore@hotmail.com>
2023-11-17 19:42:19 -0500

    Added an int interface
    Signed-off-by: Nick Moore <nick_r_moore@hotmail.com>

--- src/Arithmetic/IArithmeticOperations.cs
+++ src/Arithmetic/IArithmeticOperations.cs
@@ -6,6 +6,7 @@ namespace ArithmeticLibrary
     public interface IArithmeticOperations
     {
         float Add (float num1, float num2);
+        int Add (int num1, int num2);
         float Subtract (float num1, float num2);
         float Divide (float num1, float num2);
         float Multiply (float num1, float num2);
